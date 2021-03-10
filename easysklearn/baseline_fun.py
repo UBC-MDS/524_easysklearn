06a83d977469c7c12c7c43cfa022c2d120b05086
yzr96 <eric.yu@usask.ca>
2021-03-10 14:36:21 -0800

    fixed the flake8

--- easysklearn/baseline_fun.py
+++ easysklearn/baseline_fun.py
@@ -4,7 +4,8 @@ from sklearn.dummy import DummyClassifier, DummyRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split, cross_validate
 
-def baseline_fun(X_train, y_train, type = 'regression', metrics_1 = 'accuracy', metrics_2 = 'r2'):
+
+def baseline_fun(X_train, y_train, type='regression', metrics_1='accuracy', metrics_2='r2'):
     """
     Gives the scoring metrics of sklearn DummyRegressor and LinearRegression or sklearn DummyClassifier and LogisticRegression.
 
@@ -12,21 +13,21 @@ def baseline_fun(X_train, y_train, type = 'regression', metrics_1 = 'accuracy', 
     ----------
     X_train : Pandas DataFrame
               The train set dataframe.
-    
+
     y_train : Pandas DataFrame
               The target of train set dataframe.
-    
+
     type: string
             What kind of supervised machine learning to use, regression or classification:
-            
+
             If “regression”, then DummyRegressor and LinearRegression would be used.
 
             If “classification", then DummyClassifier and LogisticRegression would be used.
-    
+
     metrics_1: string
             What kind of score metrics to use for classification problem, the default one is accuracy. 
             Use sorted(sklearn.metrics.SCORERS.keys()) to get valid options.
-    
+
     metrics_2: string
             What kind of score metrics to use for regression problem, the default one is r2
             Use sorted(sklearn.metrics.SCORERS.keys()) to get valid options.
@@ -35,42 +36,48 @@ def baseline_fun(X_train, y_train, type = 'regression', metrics_1 = 'accuracy', 
     -------
     score : Pandas DataFrame
                   The DataFrame contains the mean of fit time, score time, training score and validation score by 5-fold cross validation for both two models.
-    
+
     Examples
     --------
     >>> from easysklearn import baseline_fun
-    
+
     >>> baseline_fun(x_train, y_train, type = 'regression', metrics = 'neg_root_mean_squared_error')
     """
 
     # input test
     if not isinstance(X_train, pd.DataFrame):
         raise TypeError('Input X should be a data frame')
-    
+
     if not isinstance(y_train, pd.DataFrame):
         raise TypeError('Input y should be a data frame')
 
     if type not in ['regression', 'classification']:
-        raise TypeError('Please check what kind of supervised machine learning to use, regression or classification')
+        raise TypeError(
+            'Please check what kind of supervised machine learning to use, regression or classification')
 
     if metrics_1 not in sklearn.metrics.SCORERS.keys():
-        raise KeyError('Please check sklearn.metrics.SCORERS.keys() to get valid options')
- 
+        raise KeyError(
+            'Please check sklearn.metrics.SCORERS.keys() to get valid options')
+
     if metrics_2 not in sklearn.metrics.SCORERS.keys():
-        raise KeyError('Please check sklearn.metrics.SCORERS.keys() to get valid options')
-    
-    
+        raise KeyError(
+            'Please check sklearn.metrics.SCORERS.keys() to get valid options')
+
     # fit data into the model
     score = {}
     if type == 'regression':
-            dr_score = cross_validate(DummyRegressor(), X_train, y_train, return_train_score=True, scoring=metrics_2)
-            lr_score = cross_validate(LinearRegression(), X_train, y_train, return_train_score=True, scoring=metrics_2)
-            score["DummyRegressor"] = pd.DataFrame(dr_score).mean()
-            score["LinearRegression"] = pd.DataFrame(lr_score).mean()
+        dr_score = cross_validate(
+            DummyRegressor(), X_train, y_train, return_train_score=True, scoring=metrics_2)
+        lr_score = cross_validate(LinearRegression(
+        ), X_train, y_train, return_train_score=True, scoring=metrics_2)
+        score["DummyRegressor"] = pd.DataFrame(dr_score).mean()
+        score["LinearRegression"] = pd.DataFrame(lr_score).mean()
     if type == 'classification':
-            dc_score = cross_validate(DummyClassifier(), X_train, y_train, return_train_score=True, scoring=metrics_1)
-            lr_score = cross_validate(LogisticRegression(), X_train, y_train, return_train_score=True, scoring=metrics_1)
-            score["DummyClassifier"] = pd.DataFrame(dc_score).mean()
-            score["LogisticRegression"] = pd.DataFrame(lr_score).mean()
-    
+        dc_score = cross_validate(
+            DummyClassifier(), X_train, y_train, return_train_score=True, scoring=metrics_1)
+        lr_score = cross_validate(LogisticRegression(
+        ), X_train, y_train, return_train_score=True, scoring=metrics_1)
+        score["DummyClassifier"] = pd.DataFrame(dc_score).mean()
+        score["LogisticRegression"] = pd.DataFrame(lr_score).mean()
+
     return pd.DataFrame(score)
--- easysklearn/feature_select.py
+++ easysklearn/feature_select.py
@@ -2,20 +2,21 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+
 def feature_select(X_train, y_train, threshold=0.05):
     """
     Performs forward selection of features in the data by starting with an empty model, 
     and iteratively adds features that improve the model's score. The algorithm stops once 
     the increase of the accuracy from an additional features is smaller than the threshold.
-    
+
     Parameters
     ----------
     X_train : Pandas DataFrame
               The training set of the data.
-    
+
     y_train : Pandas DataFrame
               The target of the data.
-    
+
     threshold : int (default=None)
                 user input threshold used for stopping criteria.
     Returns
@@ -26,42 +27,44 @@ def feature_select(X_train, y_train, threshold=0.05):
     --------
     >>> from easysklearn import feature_select
     >>> from sklearn.linear_model import LinearRegression
-    
+
     >>> feature_select(X_train, y_train, threshold=None)
     """
- 
+
     # exception handling
     if len(X_train.shape) != 2:
         raise ValueError('X_train must be 2-dimensional.')
 
     if len(y_train.shape) != 1:
         raise ValueError('y_train must be 1-dimensional.')
-        
+
     if X_train.shape[0] != y_train.shape[0]:
         raise ValueError(f'X_train and y_train have a dimensions mismatch: '
-            '[{X_train.shape[0]}, {y_train.shape[0]}]')
-    
+                         '[{X_train.shape[0]}, {y_train.shape[0]}]')
+
     initial_features = X_train.columns.tolist()
     best_features = []
     scores = []
     max_features = X_train.shape[1]
     previous = 0
-    
+
     for j in range(0, max_features):
         remaining_features = list(set(initial_features)-set(best_features))
-        temp = pd.Series(index=remaining_features, dtype= 'float64')
+        temp = pd.Series(index=remaining_features, dtype='float64')
         for temp_feature in remaining_features:
-            
-            model = LinearRegression().fit(X_train[best_features+[temp_feature]], y_train)
-            temp[temp_feature] = 1 - model.score(X_train[best_features+[temp_feature]], y_train)
-        
+
+            model = LinearRegression().fit(
+                X_train[best_features+[temp_feature]], y_train)
+            temp[temp_feature] = 1 - \
+                model.score(X_train[best_features+[temp_feature]], y_train)
+
         min_value = temp.min()
         scores.append(min_value)
-        
-        # Stopping Criteria: Based on threshold 
+
+        # Stopping Criteria: Based on threshold
         if (j > 1):
             previous = sorted(np.array(scores))[1]
             if(((previous - np.min(scores)) / previous) >= threshold):
                 break
-        best_features.append(temp.idxmin())   
-    return best_features 
+        best_features.append(temp.idxmin())
+    return best_features
